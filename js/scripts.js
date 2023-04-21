21105a843e292132a99fae24e19480e1cb863b8f
Aidan Williams <aidanwilliams01@gmail.com>
2023-04-21 16:16:03 -0700

    remove console.log()

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 window.onload = function() {
   function getAndSetFormValues() {
     const shoutInput = document.querySelector("input").value;
-    console.log(shoutInput);
     document.querySelector("p").innerText = shoutInput.toUpperCase();
   }
   let form = document.querySelector("form");
